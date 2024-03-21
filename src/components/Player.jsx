905251540a1618252534283930d13ace3a3246ab
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-03-21 18:27:01 +0530

    Manipulating the DOM via Refs (Not the best way)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,6 +6,7 @@ export default function Player() {
 
   function handleClick() {
     setPlayerName(refPlayerName.current.value)
+    refPlayerName.current.value=''
   }
   return (
     <section id="player">
