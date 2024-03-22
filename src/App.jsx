1a834f8cb07739e8f30ec39cca6bb0a0a0ee64d9
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-03-22 11:58:53 +0530

    Adding Challenges to the Demo Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallange from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallange title={"Little Easy"} targetTime={1}/>
+        <TimerChallange title={"Not Easy"} targetTime={5}/>
+        <TimerChallange title={"Getting Tough"} targetTime={10}/>
+        <TimerChallange title={"Pros Only"} targetTime={15}/>
+      </div>
     </>
   );
 }
