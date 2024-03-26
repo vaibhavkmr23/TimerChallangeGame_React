0f0ca904322dcfeedd86a3eb84f15ae98967ea6a
Vaibhav Kumar Jha <vaibhavkumarjha46@gmail.com>
2024-03-26 13:36:32 +0530

    Enhancing the Demo App "Result Modal"

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -10,11 +10,13 @@ const ResultModal = forwardRef(function ResultModal({ remainingTime, targetTime,
     }
   })
   const userLost = remainingTime <= 0
-  const formattedRemainingTime = (remainingTime/3).toFixed(2)
+  const formattedRemainingTime = (remainingTime/1000).toFixed(2)
+  const score = Math.round((1 - remainingTime/(targetTime*1000))*100)
   
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2> Your Score {score}</h2>}
       <p>
         Target time was <strong>{targetTime} seconds</strong>
       </p>
